ebc02a5bfb893e165b2a7995a92bfa241492fea1
sightondrugs <92496304+sightondrugs@users.noreply.github.com>
2025-08-13 05:20:52 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'), // or 'admin/index.html' if you move it into a folder
+      },
+    },
+  },
+})
